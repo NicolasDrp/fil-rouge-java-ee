90262f646db7a54222a1da3269b70493cefd9290
NicolasDrp <drapier.nicolas07@gmail.com>
2023-01-03 10:56:49 +0100

    class achat réparé

--- Doudouxshop/src/main/java/co/simplon/Doudouxshop/Achat.java
+++ Doudouxshop/src/main/java/co/simplon/Doudouxshop/Achat.java
@@ -1,7 +1,6 @@
 package co.simplon.Doudouxshop;
 
-import java.util.Date;
-
+import java.util.Calendar;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
@@ -16,199 +15,178 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "achat")
-
 public class Achat {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "idachat")
+  private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "idachat", unique = true)
-	private int idachat;
+  @ManyToOne
+  @JoinColumn(name = "idproduit")
+  private Produit produit;
 
-	@ManyToOne
-	@JoinColumn(name = "idproduit")
-	private Produit idproduit;
+  @Column(name = "fournisseur")
+  private String fournisseur;
 
-	@Column(name = "fournisseur", length = 30)
-	private String fournisseur;
+  @Column(name = "date")
+  private Calendar date;
 
-	@Column(name = "date")
-	private Date date;
-
-	@Column(name = "nbrachat")
-	private int nbrachat;
-
-	/**
-	 * @param idproduit
-	 * @param fournisseur
-	 * @param nbrachat
-	 */
-	/*public Achat(Produit idproduit, String fournisseur, int nbrachat) {
-		this.idproduit = idproduit;
-		this.fournisseur = fournisseur;
-		this.nbrachat = nbrachat;
-	}*/
-
-
-	public Achat() {
-
-	}
-	
-	public Achat(int idproduit, String fournisseur, int nbrachat,Date date) {
-
-	}
-	
-		
-
-	
-	public void ajoutAchat(int idproduit, String fournisseur, int nbrachat,Date date) {
-
-		// The EntityManager class allows operations such as create, read, update,
-		// delete
-		EntityManager em = utils.JPA.getEntityManager();
-		// Used to issue transactions on the EntityManager
-		EntityTransaction et = null;
-
-		try {
-			// Get transaction and start
-			et = em.getTransaction();
-			et.begin();
-
-			// Create and set values 
-			Achat achat = new Achat(idproduit,fournisseur,nbrachat,date);
-
-			// Save the customer object
-			em.persist(achat);
-			et.commit();
-		} catch (Exception ex) {
-			// If there is an exception rollback changes
-			if (et != null) {
-				et.rollback();
-			}
-			ex.printStackTrace();
-		} finally {
-			// Close EntityManager
-			em.close();
-		}
-	}
-	
-	
-	public void ajoutProduit( String fournisseur, int nbrachat) {
-
-		// The EntityManager class allows operations such as create, read, update,
-		// delete
-		EntityManager em = utils.JPA.getEntityManager();
-		// Used to issue transactions on the EntityManager
-		EntityTransaction et = null;
-
-		try {
-			// Get transaction and start
-			et = em.getTransaction();
-			et.begin();
-
-			// Create and set values 
-			Achat achat = new Achat();
-			setDate(date);
-			setFournisseur(fournisseur);
-			setNbrachat(nbrachat);
-			
-			
-
-			// Save the customer object
-			em.persist(achat);
-			et.commit();
-			
-			System.out.println(getFournisseur());
-			
-		} catch (Exception ex) {
-			// If there is an exception rollback changes
-			if (et != null) {
-				et.rollback();
-			}
-			ex.printStackTrace();
-		} finally {
-			// Close EntityManager
-			em.close();
-		}
-	}
-	
-
-	public void ajouterStock(int idproduit, String fournisseur, int nbrachat) {
-
-		try {
-			EntityManager em = utils.JPA.getEntityManager();
-
-			em.getTransaction().begin();
-
-			Produit produitMaj = em.find(Produit.class, idproduit);
-			produitMaj.setQuantite(produitMaj.getQuantite() + nbrachat);
-
-			em.getTransaction().commit();
-			em.close();
-		} catch (NoResultException e) {
-			e.printStackTrace();
-			System.out.println("Le produit n'existe pas");
-		}
-	}
-
-	
-	
-	
-	
-	
-	/**
-	 * @return the produit
-	 */
-	public Produit getIdproduit() {
-		return idproduit;
-	}
-
-	/**
-	 * @param produit the produit to set
-	 */
-	public void setIdproduit(Produit idproduit) {
-		this.idproduit = idproduit;
-	}
-
-	/**
-	 * @return the fournisseur
-	 */
-	public String getFournisseur() {
-		return fournisseur;
-	}
-
-	/**
-	 * @param fournisseur the fournisseur to set
-	 */
-	public void setFournisseur(String fournisseur) {
-		this.fournisseur = fournisseur;
-	}
-
-	/**
-	 * @return the date
-	 */
-	public Date getDate() {
-		return date;
-	}
-
-	/**
-	 * @param date the date to set
-	 */
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	/**
-	 * @return the nbrachat
-	 */
-	public int getNbrachat() {
-		return nbrachat;
-	}
-
-	/**
-	 * @param nbrachat the nbrachat to set
-	 */
-	public void setNbrachat(int nbrachat) {
-		this.nbrachat = nbrachat;
-	}
-
+  @Column(name = "nbrachat")
+  private Integer nbrAchat;
+    
+  
+  /**
+ * @param produit
+ * @param fournisseur
+ * @param date
+ * @param nbrAchat
+ */
+public Achat(Produit produit, String fournisseur, Calendar date, Integer nbrAchat) {
+	super();
+	this.produit = produit;
+	this.fournisseur = fournisseur;
+	this.date = date;
+	this.nbrAchat = nbrAchat;
 }
+
+/**
+ * @param fournisseur
+ * @param date
+ * @param nbrAchat
+ */
+public Achat(String fournisseur, Calendar date, Integer nbrAchat) {
+	super();
+	this.fournisseur = fournisseur;
+	this.date = date;
+	this.nbrAchat = nbrAchat;
+}
+
+
+
+
+//ajouter le stock dans  la table produit
+public void ajouterStock(int idproduit, String fournisseur, int nbrachat) {
+
+	try {
+		EntityManager em = utils.JPA.getEntityManager();
+
+		em.getTransaction().begin();
+
+		Produit produitMaj = em.find(Produit.class, idproduit);
+		produitMaj.setQuantite(produitMaj.getQuantite() + nbrachat);
+
+		em.getTransaction().commit();
+		//em.close();
+	} catch (NoResultException e) {
+		e.printStackTrace();
+		System.out.println("Le produit n'existe pas");
+	}
+}
+
+
+//ajouter les valeurs a la base de donnée
+public void ajoutProduit() {
+
+	// The EntityManager class allows operations such as create, read, update,
+	// delete
+	EntityManager em = utils.JPA.getEntityManager();
+	// Used to issue transactions on the EntityManager
+	EntityTransaction et = null;
+
+	try {
+		// Get transaction and start
+		et = em.getTransaction();
+		et.begin();
+
+		// Create and set values 
+		Achat achat = new Achat(this.produit,this.fournisseur, this.date,this.nbrAchat);
+
+		// Save the customer object
+		em.persist(achat);
+		et.commit();
+	} catch (Exception ex) {
+		// If there is an exception rollback changes
+		if (et != null) {
+			et.rollback();
+		}
+		ex.printStackTrace();
+	} finally {
+		// Close EntityManager
+		em.close();
+	}
+}  
+  
+  
+/**
+ * @return the id
+ */
+public Long getId() {
+	return id;
+}
+
+/**
+ * @param id the id to set
+ */
+public void setId(Long id) {
+	this.id = id;
+}
+
+/**
+ * @return the produit
+ */
+public Produit getProduit() {
+	return produit;
+}
+
+/**
+ * @param produit the produit to set
+ */
+public void setProduit(Produit produit) {
+	this.produit = produit;
+}
+
+/**
+ * @return the fournisseur
+ */
+public String getFournisseur() {
+	return fournisseur;
+}
+
+/**
+ * @param fournisseur the fournisseur to set
+ */
+public void setFournisseur(String fournisseur) {
+	this.fournisseur = fournisseur;
+}
+
+/**
+ * @return the date
+ */
+public Calendar getDate() {
+	return date;
+}
+
+/**
+ * @param date the date to set
+ */
+public void setDate(Calendar date) {
+	this.date = date;
+}
+
+/**
+ * @return the nbrAchat
+ */
+public Integer getNbrAchat() {
+	return nbrAchat;
+}
+
+/**
+ * @param nbrAchat the nbrAchat to set
+ */
+public void setNbrAchat(Integer nbrAchat) {
+	this.nbrAchat = nbrAchat;
+}
+  
+  
+}
+
--- Doudouxshop/src/main/java/co/simplon/Doudouxshop/Main.java
+++ Doudouxshop/src/main/java/co/simplon/Doudouxshop/Main.java
@@ -1,5 +1,6 @@
 package co.simplon.Doudouxshop;
 
+import java.util.Calendar;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -34,6 +35,8 @@ public class Main {
 							+ "\r3. Ajouter un produit" + "\r4. Modifier un produit" + "\r5. Supprimer un produit"
 							+ "\r6. Ajouter du stock" + "\r7. Afficher l'historique des livraisons de stock "
 							+ "\r8. Retour au menu de connexion" + "\r9. Quitter");
+			
+			
 			selection = 0;
 			while (selection == 0 || selection < 1 || selection > 9) {
 				try {
@@ -46,18 +49,30 @@ public class Main {
 				}
 			}
 			switch (selection) {
+			
+			
 			case 1:// Afficher les produit disponible // marche
+				
+				
 				Produit list = new Produit();
 				list.getProduits();
 				//main(null);
 				break;
+				
+				
 			case 2:// chercher un produit en particulier // marche
+				
+				
 				Produit prod = new Produit();
 				String nomprod = "t-shirt j-m";
 				prod.getProduit(nomprod);
 				//main(null);
 				break;
+				
+				
 			case 3:// Ajouter un produit // marche
+				
+				
 				System.out.println("Nom du produit: ");
 				String nom = scan.nextLine();
 
@@ -70,7 +85,11 @@ public class Main {
 				Produit produit = new Produit(nom, prix, quantite);
 				produit.ajoutProduit();
 				break;
+				
+				
 			case 4:// modifier un produit // marche
+				
+				
 				System.out.println("Nom du produit: ");
 				String nommodif = scan.nextLine();
 				
@@ -81,56 +100,87 @@ public class Main {
 
 				modif.changerNom(nommodif, nvnom);
 				break;
+				
+				
 			case 5:// Supprimer un produit // marche
+				
+				
 				System.out.println("Quel produit souhaitez vous supprimer ?");
 				String nomsup = scan.nextLine();
 
 				Produit sup = new Produit();
 				sup.supprimerProduit(nomsup);
 				break;
+				
+				
 			case 6:// Ajouter du stock à un produit // marche
+				
+				
 				System.out.println("Entrez l'id du produit");
 				int id = scan.nextInt();
 				scan.nextLine();
 				
+				Calendar date = Calendar.getInstance();
+			 	Produit prodId = new Produit();
+			 	prodId.setId(id);
+				
 				System.out.println("Entrez le nom du fournisseur du produit");
 				String nomfour = scan.nextLine();
 				
-				System.out.println("Entrez le nombre de produit livré du produit");
-				int nbrprod = scan.nextInt();
-				scan.nextLine();
 				
-				Achat stock = new Achat();
-				stock.ajouterStock(id,nomfour,nbrprod);
+				
+				int nbrprod = 0;
+				while (nbrprod == 0 || nbrprod < 1) {
+					try {
+						System.out.println("Entrez le nombre de produit livré");
+						nbrprod = scan.nextInt();
+						scan.nextLine();
+					} catch (InputMismatchException e) {
+						System.out.println("Veuillez entrer un chiffre.");
+						scan.nextLine();
+					}
+				}
+				
+				try {
+					Achat achat = new Achat(prodId,nomfour,date, nbrprod);
+					
+					achat.ajouterStock(id,nomfour,nbrprod);
+					
+					achat.ajoutProduit();
+				} catch (NullPointerException e) {
+					System.out.println("Vous essayer d'ajouter du stock à un produit qui n'existe pas ou plus \n"
+							+ "RETOUR AU MENU");
+				}
+				
 				break;
+				
 			case 7:// Afficher l'historique des achat
 
-				//Achat achat = new Achat();
-				//System.out.println(achat.getFournisseur());
-				//achat.ajoutAchat(1,"oracle",10,new Date());
-				Achat achat = new Achat();
-				achat.ajoutProduit("oracle",10);
-				System.out.println(achat.getNbrachat());
-				achat.setFournisseur("tonpere");
-				System.out.println(achat.getFournisseur());
+				
+				
 				
 				break;
 
 			case 8:// Retour au menu de connexion
+				
 				main(null);
 				break;
 
 			case 9:// Quitter
+				
 				System.out.println("Au revoir !");
 				System.exit(0);
+				
 				break;
 
 			default:
+				
 				break;
 
 			}
 
 			break;
+			
 		case 2:// CONNEXION CLIENT
 			/*
 			 * vente // produit // panier
@@ -140,6 +190,8 @@ public class Main {
 					"Que voulez vous faire?" + "\r1. Afficher les produit disponible" + "\r2. Rechercher un produit"
 							+ "\r3. Ajouter un produit au panier" + "\r4. Supprimer un produit" + "\r5. Afficher le panier"
 							+ "\r6. Passer commande" + "\r7. Retour au menu de connexion" + "\r8. Quitter");
+			
+			
 			
 			selection = 0;
 			while (selection == 0 || selection < 1 || selection > 9) {
@@ -154,29 +206,59 @@ public class Main {
 			}
 			
 			switch (selection) {
+			
+			
 			case 1:
+				
+				
 				//afficher la liste de tout les produit
 				Produit list = new Produit();
 				list.getProduits();
 				
 				break;
-			case 2:
-				//rechercher un produit en particulier
 				
+				
+			case 2:
+				
+				
+				//rechercher un produit en particulier
+				 Calendar date = Calendar.getInstance();
+				 	Produit produit = new Produit();
+				 	produit.setId(1);
+					Achat achat = new Achat(produit,"testtage",date, 43);
+					achat.ajoutProduit();
+					System.out.println(achat.getNbrAchat());
+					achat.setFournisseur("tonpere");
+					System.out.println(achat.getFournisseur());
 				break;
+				
+				
 			case 3:
 				//ajouter un produit au panier
 				
+				
 				break;
+				
+				
 			case 4:
+				
+				
 				//supprimer un produit au panier
 				
 				break;
+				
+				
 			case 5:
+				
+				
 				//afficher le panier
 				
 				break;
+				
+				
 			case 6:
+				
+				
 				//passer commande
 				
 				break;
@@ -186,7 +268,11 @@ public class Main {
 			}
 			
 			break;
+			
+			
 		case 3:
+			
+			
 			System.out.println("Au revoir !");
 			// quitter
 			System.exit(0);
